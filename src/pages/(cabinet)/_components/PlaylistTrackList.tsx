0c52eea9b37a15c24011000b11a35c9595999ded
Zoody <reopard506@gmail.com>
2024-09-26 17:07:35 +0900

    [🔨CHORE]     : 배포를 위해 tscofing 옵션 수정

--- src/pages/(cabinet)/_components/PlaylistTrackList.tsx
+++ src/pages/(cabinet)/_components/PlaylistTrackList.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 interface PlaylistTrackListProps {
   tracks: Track[];
-  playlistId: string;
+  playlistId?: string;
   isOwn: boolean;
   className?: string;
   trackListType: "LIST" | "QUEUE";
