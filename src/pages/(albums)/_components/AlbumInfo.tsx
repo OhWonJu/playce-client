854e1fb0e2cf2d86346a3cfa2c323f3bd1fa6593
Zoody <reopard506@gmail.com>
2024-10-11 14:18:13 +0900

    [🐛FIX]       : 오사용한 tag 수정

--- src/pages/(albums)/_components/AlbumInfo.tsx
+++ src/pages/(albums)/_components/AlbumInfo.tsx
@@ -44,21 +44,21 @@ const AlbumInfo = ({
     <AlbumInfoBox>
       <span className="font-extrabold text-3xl">{albumName}</span>
       <span className="font-bold">{artistName}</span>
-      <a className="text-zinc-400 font-medium text-sm">
+      <p className="text-zinc-400 font-medium text-sm">
         {albumType && `${albumType} • `}
         {releasedAt && `${format(releasedAt, DATE_FORMAT)}`}
-      </a>
+      </p>
       {genres && (
-        <a className="text-zinc-400 font-medium text-sm">
+        <p className="text-zinc-400 font-medium text-sm">
           {genresContent.join(" • ")}
-        </a>
+        </p>
       )}
       <div className="flex space-x-2">
-        <a className="text-zinc-400 font-medium text-sm">
+        <p className="text-zinc-400 font-medium text-sm">
           {tracks && `${tracks?.length}곡 • ${Math.round(totalTimes / 60)}분`}
           {songCount && `${songCount}곡`}
           {totalPlayTime && ` • ${convertTime(totalPlayTime, "number")[0]}분`}
-        </a>
+        </p>
       </div>
     </AlbumInfoBox>
   );
